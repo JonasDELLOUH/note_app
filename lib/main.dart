5a7ae2334b64a19d2b8a6436bf6c6c3030c72cf9
JonasDELLOUH <jdellouh1@gmail.com>
2022-06-21 11:24:28 +0200

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:note_app/screens/sign_in.dart';
@@ -7,12 +8,21 @@ import 'helpers/helper_functions.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  FirebaseDatabase.instance.setPersistenceEnabled(true);
+  final connectedRef = FirebaseDatabase.instance.ref(".info/connected");
+  connectedRef.onValue.listen((event) {
+    final connected = event.snapshot.value as bool? ?? false;
+    if (connected) {
+      debugPrint("Connected.");
+    } else {
+      debugPrint("Not connected.");
+    }
+  });
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  MyApp({Key? key}) : super(key: key);
   @override
   State<MyApp> createState() => _MyAppState();
 }
